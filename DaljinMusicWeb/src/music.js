14a8dd862ae7eadd0262c660c5b0fc951e3f9397
JangDaljin <toyyj15@naver.com>
2019-08-29 18:02:16 +0900

    초기세팅

--- DaljinMusicWeb/src/music.js
+++
@@ -1,6 +0,0 @@
-import React , { Component } from 'react'
-import ReactDom from 'react-dom'
-import MusicView from './musicView'
-
-
-ReactDom.render(<MusicView /> , document.getElementById('root'));
