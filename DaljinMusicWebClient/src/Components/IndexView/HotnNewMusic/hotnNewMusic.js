4fff63225cbe45f15f318d7f9098178b260542a7
JangDaljin <toyyj15@naver.com>
2019-12-19 18:59:32 +0900

    저장

--- DaljinMusicWebClient/src/Components/IndexView/HotnNewMusic/hotnNewMusic.js
+++ DaljinMusicWebClient/src/Components/IndexView/HotnNewMusic/hotnNewMusic.js
@@ -18,7 +18,10 @@ class HotAndNewMusic extends Component {
         
 
         this.state = {
-            currentIndex : 0
+            currentIndex : 0,
+            imageShow : false,
+            image_X : 0,
+            imgae_Y : 0,
         }
     }
 
@@ -26,22 +29,33 @@ class HotAndNewMusic extends Component {
         this.props.HotAndNewActions.fetchHotAndNew();
     }
 
+    imageShow = (e) => {
+        this.setState({'imageShow' : true})
+    }
+
+    imageMove = (e) => {
+        console.log(`X : ${e.nativeEvent.x} , Y : ${e.nativeEvent.y}`)
+        this.setState({'image_X' : e.nativeEvent.x , 'image_Y' : e.nativeEvent.y})
+    }
+
+    imageClose = (e) => {
+        this.setState({'imageShow' : false})
+    }
+
     render () {
         return (
+            <React.Fragment>
             <div className={cn('hotandnew')}>
                 <div className={cn('hotandnew-title')}>
                     <span style={{color:'#FF5A5A'}}>HOT</span><span style={{color:'#9887b9'}}>&nbsp;&&nbsp;</span><span style={{color:'#069'}}>NEW</span>
                 </div>
 
                 <div className={cn('hotandnew-content')}>
-                    <div className={cn('hotandnew-img-wrap')}>
-                        <div className={cn('hotandnew-img')} style={{backgroundImage:`url('${this.props.list.getIn([this.state.currentIndex , 'music' , 'album' , 'albumImgUri'])}')`}}></div>
-                    </div>
                     <div className={cn('hotandnew-list')}>
                         {this.props.list.map(
                             (value , index) => (
 
-                                <div className={cn('hotandnew-list-item')} key={index} onMouseOver={(e) => { this.setState({'currentIndex' : index})}}>
+                                <div className={cn('hotandnew-list-item')} key={index} onMouseOver={this.imageShow} onMouseOut={this.imageClose} onMouseMove={this.imageMove}>
                                     <div className={cn('hotandnew-list-item-info')}>
                                         <div>{value.getIn(['music' , 'singer' , 'name'])}</div>
                                         <div>{value.getIn(['music' , 'song'])}</div>
@@ -62,6 +76,18 @@ class HotAndNewMusic extends Component {
                     </div>
                 </div>
             </div>
+
+            <div className={cn('hotandnew-img-wrap')}>
+                <div className={cn('hotandnew-img')} style={
+                    {
+                        backgroundImage:`url('${this.props.list.getIn([this.state.currentIndex , 'music' , 'album' , 'albumImgUri'])}')`,
+                        top : this.state.image_Y,
+                        left : this.state.image_X,
+                        visibility : this.state.imageShow ? 'true' : 'false',
+                    }}>
+                </div>
+            </div>
+            </React.Fragment>
 
         )
     }
